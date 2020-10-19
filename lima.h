3bab965e796d712bc6fc0a863f89f0192862627f
Pedro Rodrigues de Lima <pee.rodrigues360@gmail.com>
2020-10-19 19:37:26 -0300

    Timer modo normal, inicialização de pinos, teste EEPROM

--- lima.h
+++ lima.h
@@ -1,6 +1,27 @@
 #ifndef LIMA_H_
 #define LIMA_H_
 
+/* atraso_timer0:
+ * Gera um atraso relativo a n contagens com Timer0 em modo normal
+ * n = 256 - ROUND((FREQ_TIMER/PRESCALER)*ATRASO)
+ */
+void atraso_timer0(volatile unsigned char n){
+	// O Timer deve ser configurado previamente conforme necessário
+	TCNT0 = n;						// Inicializa o timer
+	while((TIFR0 & (1 << 0)) == 0);	// Espera o flag de overflow
+	TIFR0 |= (1 << 0);				// Zera o flag de overflow
+}
+
+/* atraso_timer1:
+ * Gera um atraso relativo a n contagens com Timer0 em modo normal
+ * n = 65536 - ROUND((FREQ_TIMER/PRESCALER)*ATRASO)
+ */
+void atraso_timer1(volatile unsigned short n){
+	TCNT1 = n;
+	while((TIFR1 & (1 << 0)) == 0);
+	TIFR1 |= (1 << 0);
+}
+
 /* atraso_timer0_ctc:
  * Gera um atraso relativo a n contagens com Timer0 em modo CTC
  * n = ROUND((FREQ_TIMER/PRESCALER)*ATRASO)
@@ -16,7 +37,7 @@ void atraso_timer0_ctc(volatile unsigned char n){
  * Gera um atraso relativo a n contagens com Timer1 em modo CTC
  * n = ROUND((FREQ_TIMER/PRESCALER)*ATRASO)
  */
-void atraso_timer1_ctc(volatile unsigned char n){
+void atraso_timer1_ctc(volatile unsigned short n){
 	// O Timer deve ser zerado e configurado previamente conforme necessário
 	OCR1A = n;
 	while((TIFR1 & (1 << 1)) == 0);	// Espera o flag de overflow
@@ -52,7 +73,6 @@ volatile unsigned char debounce(volatile unsigned char tecla){
 void escreve_EEPROM(volatile unsigned short endereco, volatile unsigned char dado){
 	// EEPE: 1 -> escrita	0 -> leitura
 	while(EECR & (1 << EEPE));			// Espera a última escrita terminar
-	EECR = (0 << EEPM1) | (0 << EEPM0);	// Escolhe o modo atômico (00, apaga e escreve de uma vez)
 	EEAR = endereco;
 	EEDR = dado;
 	EECR |= (1 << EEMPE);				// Liga o Master Program Enable
--- main.c
+++ main.c
@@ -8,6 +8,22 @@
 #define ATR_800 0
 #define ATR_1200 1
 
+// Modos do Timer0
+#define T0OVR 0x00	// Modo overflow
+#define T0CTC 0x02	// Modo CTC
+
+// Modos do Timer1
+#define T1OVR 0x00	// Modo overflow
+#define T1CTC 0x04	// Modo CTC
+
+// Prescalers (comuns para os timers)
+#define PRE_TIMER_OFF 0x00
+#define PRE_1 0x01
+#define PRE_8 0x02
+#define PRE_64 0x03
+#define PRE_256 0x04
+#define PRE_1024 0x05
+
 int main(void){
 	/*// Relação de clientes
 	short lista_clientes [11] =
@@ -55,25 +71,51 @@ int main(void){
 	*/
 	
 	volatile unsigned char atrasos[2] = {50,75};
+	volatile unsigned char dado;
 	
+	// EEPROM
+	EECR &= (~(1 << EEPM1) & ~(1 << EEPM0));	// Escolhe o modo atômico (00)
 	escreve_EEPROM(0x02,0x84);
 	escreve_EEPROM(0x11,0xAB);
+	escreve_EEPROM(0x00,atrasos[0]);
+	// Le o atraso da EEPROM
+	dado = le_EEPROM(0x00);
 	
 	// Configuração Timer0
-	TCCR0A = 0x02;			// Modo CTC
-	TCCR0B = 0x04;			// Prescaler de 256 (FREQ_CPU = 16MHz -> FREQ TIMER = 62.5kHz)
+	TCCR0A = T0OVR;			// Modo do Timer0
+	TCCR0B = PRE_256;		// Prescaler de 256 (FREQ_CPU = 16MHz -> FREQ TIMER = 62.5kHz)
 	TIFR0 |= (1 << 1);		// Zera o flag de overflow do Timer0
 	
 	// Configuração Timer1
-	TCCR1A = 0x04;			// Modo CTC
-	TCCR1B = 0x04;			// Prescaler de 256 (FREQ_CPU = 16MHz -> FREQ TIMER = 62.5kHz)
+	TCCR1A = T1OVR;			// Modo do Timer1
+	TCCR1B = PRE_256;		// Prescaler de 256 (FREQ_CPU = 16MHz -> FREQ TIMER = 62.5kHz)
 	TIFR1 |= (1 << 1);		// Zera o flag de overflow do Timer1
 
 	DDRB |= (1 << 5);		// Seta pino PB5 como output
 	DDRB |= (1 << 4);		// Seta pino PB4 como output
     
+	// Inicialização dos pinos do LCD
+	DDRB |= 0x0F;			// Seta os pinos de dados do LCD como output
+	
+	// Inicialização dos pinos do teclado
+	DDRD |=	0x0F;			// Seta as linhas do teclado como output
+	DDRD &= ~(0x70);			// Seta as colunas do teclado como input
+	
     while (1){
+		// TESTE TIMER OVERFLOW USANDO DADO DA EEPROM
+		PORTB ^= (1 << 4);
+		PORTB ^= (1 << 5);
+		atraso_timer0(256 - atrasos[ATR_1200]);
+		PORTB ^= (1 << 5);
+		atraso_timer0(256 - dado);
+
+		PORTB ^= (1 << 4);
+		PORTB ^= (1 << 5);
+		atraso_timer1(65536 - atrasos[ATR_1200]);
+		PORTB ^= (1 << 5);
+		atraso_timer1(65536 - dado);
 		
+		/* // TESTE TIMER CTC
 		PORTB &= ~(1 << 4);						// Zera o pino PB4
 		
 		PORTB ^= (1 << 5);						// Inverte o pino PB5
@@ -87,9 +129,11 @@ int main(void){
 		PORTB ^= (1 << 5);
 		TCNT1 = 0x00;							// Zera o Timer1
 		atraso_timer1_ctc(atrasos[ATR_1200]);	// Atraso de 1200us
-		PORTB ^= (1 << 5);		
-		TCNT1 = 0x00;							// Zera o Timer1 (?)
+		PORTB ^= (1 << 5);
+		TCNT1 = 0x00;
 		atraso_timer1_ctc(atrasos[ATR_800]);	// Atraso de 800us
+		*/
+
     }
 }
 
